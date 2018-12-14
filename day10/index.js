6c3403b1c9d978f475174d795a147d7a23322a25
Michael Faißt <michael.faisst@senseforce.io>
2018-12-14 12:04:59 +0100

    puzzle 1 of day 10

---
+++ day10/index.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+const util = require("util");
+fs.readFileAsync = util.promisify(fs.readFile);
+
+const solution = require("./solution");
+solution.calculateSolution1();
---
+++ day10/solution.js
@@ -0,0 +1,88 @@
+const fs = require("fs");
+
+/*
+  ------------------------
+  SOLUTION 1
+  ------------------------
+*/
+
+/**
+ * Parses all given lines and creates points out of it
+ * @param {string[]} lines
+ * @returns {{x: number, y: number, deltaX: number, deltaY: number}[]}
+ */
+const parsePoints = lines => {
+    return lines.map(line => {
+        const parsedLine = line.match(/(-?\d+)/g);
+
+        return {
+            x: +parsedLine[0],
+            y: +parsedLine[1],
+            deltaX: +parsedLine[2],
+            deltaY: +parsedLine[3]
+        };
+    });
+};
+
+/**
+ * Calculates a iteration of star movement
+ * @param {{x: number, y: number, deltaX: number, deltaY: number}[]} points
+ */
+const calculateIteration = (points, forward = true) => {
+    for (const point of points) {
+        point.x = forward ? point.x + point.deltaX : point.x - point.deltaX;
+        point.y = forward ? point.y + point.deltaY : point.y - point.deltaY;
+    }
+
+    return (
+        Math.max(...points.map(point => point.x)) +
+        Math.abs(Math.min(...points.map(point => point.x)))
+    );
+};
+
+/**
+ * Prints a star constalation to the console
+ * @param {{x: number, y: number, deltaX: number, deltaY: number}[]} points
+ */
+const print = (file, points) => {
+    const minX = Math.min(...points.map(point => point.x));
+    const maxX = Math.max(...points.map(point => point.x));
+    const minY = Math.min(...points.map(point => point.y));
+    const maxY = Math.max(...points.map(point => point.y));
+
+    const stream = fs.createWriteStream(file);
+    stream.once("open", () => {
+        for (let y = minY; y <= maxY; ++y) {
+            let line = "";
+            for (let x = minX; x <= maxX; ++x) {
+                const point = points.find(
+                    point => point.x === x && point.y === y
+                );
+                line += point == null ? "." : "#";
+            }
+
+            stream.write(line);
+            stream.write("\n");
+        }
+    });
+};
+
+const calculateSolution1 = async (iterations = 100) => {
+    const data = await fs.readFileAsync("input.txt", "UTF8");
+    const points = parsePoints(data.split("\n"));
+
+    let lastHeight;
+    let currentHeight = Infinity;
+
+    do {
+        lastHeight = currentHeight;
+        currentHeight = calculateIteration(points);
+    } while (lastHeight >= currentHeight);
+
+    calculateIteration(points, false);
+    print("output.txt", points);
+};
+
+module.exports = {
+    calculateSolution1
+};
