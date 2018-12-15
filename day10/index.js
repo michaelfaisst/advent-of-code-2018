101695e89e19abd22abd858f23b0479a19dac383
Michael Faißt <michael@codenite.at>
2018-12-15 10:54:34 +0100

    fixed wrong method name

--- day10/index.js
+++ day10/index.js
@@ -3,4 +3,4 @@ const util = require("util");
 fs.readFileAsync = util.promisify(fs.readFile);
 
 const solution = require("./solution");
-solution.calculateSolution1();
+solution.calculateSolution();
